5d6eae3aa6730581009acccaed43cbb4cb33b4c4
myshmeh <myshmeh@gmail.com>
2021-05-08 20:08:12 +0900

    fixed a bug

--- src/login-to-twitter.js
+++ src/login-to-twitter.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
-const { writeCookies } = require("./applications/cookie-service");
-const { saveAsPdf } = require("./applications/screenshot-service");
+const { writeCookies } = require("./application/cookie-service");
+const { saveAsPdf } = require("./application/screenshot-service");
 require("dotenv").config();
 
 const username = process.argv[2];
@@ -20,10 +20,12 @@ const password = process.argv[3];
     deviceScaleFactor: 1,
   });
 
-  await page.goto(process.env.TWITTER_URL, {
+  console.log('connecting to', process.env.TWITTER_URL);
+  await page.goto(`${process.env.TWITTER_URL}/login`, {
     waitUntil: "networkidle2",
   });
 
+  console.log('typing username and password');
   await page.evaluate(
     (username, password) => {
       document.querySelector(
@@ -37,15 +39,18 @@ const password = process.argv[3];
     password
   );
 
+  console.log('clicking login button');
   await Promise.all([
-    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    // page.waitForNavigation({ waitUntil: "networkidle2" }),
     page.click('div[data-testid="LoginForm_Login_Button"]'),
   ]);
 
+  console.log('writing cookie');
   const cookies = await page.cookies();
   writeCookies(cookies, username);
 
-  await savetAsPdf(page, "test");
+  console.log('screenshot..');
+  await saveAsPdf(page, "test");
 
   await browser.close();
 })();
